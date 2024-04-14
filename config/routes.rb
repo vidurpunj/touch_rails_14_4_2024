5a30f3c33cefe4a657fd64888b102c2304ea3ca0
vidur <vidur.punj@hotmail.com>
2024-04-14 20:50:31 +0530

    Added contact us path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources 'contacts'
+  resources 'about'
+  resources 'services'
+  resources 'portfolio'
+  resources 'blogs'
   root to: 'home#index'
   get "up" => "rails/health#show", as: :rails_health_check
 end
