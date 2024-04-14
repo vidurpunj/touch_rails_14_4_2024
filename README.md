73932c1e71a0cf4f7aa754c05d38cf0bab96d10a
vidur <vidur.punj@hotmail.com>
2024-04-14 19:13:36 +0530

    Resolved js and css errors

---
+++ README.md
@@ -0,0 +1,1 @@
+rails new touch_rails_14_4_2024 --skip-sprockets --skip-turbolinks --skip-javascript --skip-webpack-install -d postgresql
---
+++ app/assets/config/manifest.js
@@ -0,0 +1,12 @@
+//= link_tree ../images
+//= link_directory ../stylesheets
+//= link_tree ../stylesheets/css
+//= link_tree ../stylesheets/fonts
+//= link css/tiny-slider.css
+//= link css/tobii.min.css
+//= link css/materialdesignicons.min.css
+//= link tiny-slider.js
+//= link tobii.min.js
+//= link feather.min.js
+//= link plugins.init.js
+//= link app.js
