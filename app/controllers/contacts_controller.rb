e7bd0885f2b8f49e476a68148dce70aee23c5fe2
vidur <vidur.punj@hotmail.com>
2024-04-19 22:40:48 +0530

    flash message

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,18 @@ class ContactsController < ApplicationController
   end
 
   def create
+    @gate_in_touch = GetInTouch.new(get_in_touch_params)
+    if @gate_in_touch.save
+      flash[:message] = "you request saved we will get back to you."
+    else
+      flash[:alert] = "your request not saved."
+    end
     redirect_back fallback_location: contacts_path
+  end
+
+  private
+
+  def get_in_touch_params
+    params.require(:get_in_touch).permit(:name, :email, :subject, :comments)
   end
 end
