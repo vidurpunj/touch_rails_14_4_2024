0ffde896db3751819ee657fe95e58a1a5b6765cd
vidur <vidur.punj@hotmail.com>
2024-04-20 19:29:10 +0530

    Added the blog form

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,30 @@
 class BlogsController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      flash[:notice] = "Blog saved."
+    else
+      flash[:alert] = "Blog not saved."
+    end
+    redirect_back fallback_location: blog_path(@blog)
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :body)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_18_135301) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_20_133656) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.text "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "get_in_touches", force: :cascade do |t|
     t.string "name"
