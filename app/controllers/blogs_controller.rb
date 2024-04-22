e85b6b815cb5491525dea1ac323231cf90700d73
vidur <vidur.punj@hotmail.com>
2024-04-22 22:55:46 +0530

    create, show and update

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -9,20 +9,33 @@ class BlogsController < ApplicationController
   end
 
   def create
-    @blog = Blog.new(blog_params)
-    byebug
+    @blog = Blog.new
+    @blog.title = params[:title]
+    @blog.body = params[:body]
     if @blog.save
       flash[:notice] = "Blog saved."
     else
       flash[:alert] = "Blog not saved."
     end
-    redirect_back fallback_location: blog_path(@blog)
+    render json: { message: flash[:notice], code: 200}
   end
 
   def show
     @blog = Blog.find(params[:id])
   end
 
+  def update
+    @blog = Blog.find(params[:id])
+    @blog.title = params[:title]
+    @blog.body = params[:body]
+    if @blog.save
+      flash[:notice] = "Blog saved."
+    else
+      flash[:alert] = "Blog not saved."
+    end
+    render json: { message: flash[:notice], code: 200}
+  end
+  
   private
 
   def blog_params
